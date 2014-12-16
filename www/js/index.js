46a5d8fcf29050b5d0e7f2fe901b1cfb1c5420ab
ugur donmez <donmez@metu.edu.tr>
2014-12-16 16:30:23 +0200

    Game Hand Gamer classes were implemented.

--- www/js/index.js
+++ www/js/index.js
@@ -10,6 +10,152 @@ var name_2;
 var name_3;
 var name_4;
 
+var round = 0;
+
+
+var gameType = {
+   
+   KING : 1,
+   HEART : 2,
+   BOY : 3,
+   NO_HEART : 4,
+   GIRL : 5,
+   LAST_TWO : 6,
+   POSITIVE : 7
+   
+};
+
+// var e = gameType.KING;
+
+
+function Game() {
+   
+   this.gamers = [];
+   this.hands = [];
+   this.round = 0;
+   
+   var i;
+   for ( i = 0 ; i < 20 ; i++ ) {
+      this.hands.push(new Hand());
+   }
+   
+   this.getHands = function() {
+      return this.hands;
+   };
+   
+   this.initNames = function(array) {
+      var i;
+      
+      for (i = 0 ; i < array.length ; i++) {
+         this.gamers.push(new Gamer(array[i]));
+      }
+      
+   };
+   
+   this.getRound = function() {
+      return this.round;
+   };
+   
+   this.setRound = function(round) {
+      this.round = round;
+   };
+   
+   this.incrementRound = function() {
+      this.round += 1;
+   };
+   
+}
+
+
+function Gamer (name) {
+    
+   this.name = name;
+   this.negative_hand_no = 0;
+   this.positive_hand_no = 0;
+   
+   this.point = 0;  
+   
+   this.setNegativeHandNo = function(size) {
+      this.negative_hand_no = size;
+   };
+   
+   this.getNegativeHandNo = function () {
+      return this.negative_hand_no;
+   };
+   
+   this.setPositiveHandNo = function(size) {
+      this.positive_hand_no = size;
+   };
+   
+   this.getPositiveHandNo = function() {
+      return this.positive_hand_no;
+   };
+   
+   this.setName = function(name) {
+      this.name = name;
+   };
+   
+   this.getName = function() {
+      return this.name;
+   };
+   
+   this.increaseNegative = function() {
+      this.negative_hand_no += 1;
+   };
+   
+   this.increasePositive = function() {
+      this.positive_hand_no += 1;
+   };
+   
+   this.addPoint = function (point) {
+      this.point += point;
+   };
+   
+   this.getPoint = function () {
+      return this.point;
+   };
+   
+}
+
+function Hand() {
+   
+   this.gamer;
+   this.type;
+   this.round;
+   
+   this.setGamer = function (gamer) {
+      this.gamer = gamer;
+   };
+   
+   this.getGamer = function () {
+      return this.gamer;
+   };
+   
+   this.setType = function (type) {
+      this.type = type;
+   };
+   
+   this.getType = function () {
+      return this.type;
+   }
+   
+   this.setRound = function (round) {
+      this.round = round;
+   };
+   
+   this.getRound = function () {
+      return this.round;
+   };
+   
+   
+}
+
+
+
+
+
+
+
 
 // TODO: continue here
 function get_games_html() {
@@ -17,7 +163,7 @@ function get_games_html() {
    var str = "<div id='game' ";
    
    if ( king != 2 ) {
-      str += 
+      //str += 
    }
    
 }
@@ -47,6 +193,14 @@ function div_hide(){
 
 
 function start_hand() {
+   
+   //var n1 = new Gamer("ugur");
+   
+   //alert(n1.get_name());
+   
+   var g = new Game();
+   
+   alert(g.getHands().length);
    
    $("#div_names").show();
    
